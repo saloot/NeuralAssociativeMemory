51915c4f84fff70e3c7eb44948b67b162e7e6cb9
Amir Hesam Salavati <saloot@gmail.com>
2014-01-24 10:22:52 +0100

    Removed a folder

--- Clustered_Neural_Netwrok/Current/P_error.m
+++
@@ -1,4 +0,0 @@
-function Pe = P_error(x,LAMBDA)
-if (x==0)
-    Pe
-Pe = 1-lambda_poly(x
--- Clustered_Neural_Netwrok/Current/visualize_data.m
+++
@@ -1,5 +0,0 @@
-load('/scratch/amir/Databases/CIFAR_10/Gray_Scale_Class_1/Train_Set/Sparse Filtered/Layer_2/Pooled_3_3/Pooled_Sparse_Filtered_CIFAR_10_Gray_Class_1_Train_Layer_2.mat')
-[COEFF,SCORE,latent] = princomp(final_database_vectorized);
- SCORE2=SCORE(:,1:3);
- COEFF2=COEFF(:,1:3);
- temp = final_database_vectorized-ones(5000,1)*mean(final_database_vectorized);
