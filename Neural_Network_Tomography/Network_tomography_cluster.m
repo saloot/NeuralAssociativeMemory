3db95963d1bdba6f910b16cc8a8ede5d48183b94
Amir Hesam Salavati <saloot@gmail.com>
2014-02-12 15:09:20 +0100

    network tomography idea submit to cluster added

--- Neural_Network_Tomography/Network_tomography_cluster.m
+++ Neural_Network_Tomography/Network_tomography_cluster.m
@@ -1,15 +1,61 @@
-%---------------------In-loop Initializations--------------------------
-    theta = 0.05;   % update threshold
-   
-    p = 0.3;                        % connection probability
-    n = 200;                        % number of neurons
-    T = parameter_range(itr);                       % number of sample recordings
-    tau = 500000;                        % This is the rate according to which membrane potential drops
-    tau2 = 1*tau;
-    acc_ave = 0;                    % average number of correctly infered edges
-    err_ave = 0;                    % average number of mistakenly infered edges + missed edges
+
+%===========================INITIALIZATION=================================
+B_LLR_thr = 0;                  % the threshold usedin updating belief LLRs
+B_LLR_flag = 1;                 
+theta = 0.05;                   % update threshold  
+p = 0.3;                        % connection probability
+network_size = 200;             % number of neurons
+tau = 500000;                   % This is the rate according to which membrane potential drops
+
+q = .65*(1-1/tau)*theta/p;      % stimulus firing probability    
+
+no_averaging_itrs = 30;         % number of times we perform each simulation for the sake of averging
+
     
-    q = .65*(1-1/tau)*theta/p;                    % stimulus firing probability    
-    avg_no_edge = round(p*n);       % estimated number of edges in the graph
-    no_edges_fire = ceil(theta*n);  % estimated number of edges required for a neuron to fire
-    no_averaging_itrs = 30;         % number of times we perform each simulation for the sake of averging
+parameter_range = [700:500:5100];
+
+%------------------------Initialize the SSH Connection---------------------
+channel = sshfrommatlab('amir','lth.epfl.ch','arashmidos'); 
+%--------------------------------------------------------------------------
+
+%==========================================================================
+
+
+
+%======================LOOP OVER THE PARAMETERS============================
+for itr = 1:length(parameter_range)
+    no_samples = parameter_range(itr);       % number of sample recordings
+    
+    %--------------------Submit the Job to the Cluster---------------------    
+    command = ['cd /scratch/amir/Network_Tomography/Submit_Cluster;qsub -N "Net_tomo_',num2str(itr),...
+        '" -v n=',num2str(network_size),',T=',num2str(no_samples),',tau=',num2str(tau),',theta=',num2str(theta),...                                        
+        ',p=',num2str(p),',q=',num2str(q),',E=',num2str(no_averaging_itrs),',B_LLR_thr=',num2str(B_LLR_thr),',B_LLR_flag=',num2str(B_LLR_flag),' net_tomography.pbs'];                                            
+    [channel, result]  =  sshfrommatlabissue(channel,command);                                                 
+    %----------------------------------------------------------------------
+                                
+                                        
+    %------------------Check the success of the submission-----------------                                    
+    if (isequal(result, {''}))                                         
+        display('Unsubmitted job!');
+    else        
+        display('Job submitted successfully!');                                    
+    end 
+end
+%==========================================================================
+    
+%=========================CLOSE THE SSH SESSION============================
+channel  =  sshfrommatlabclose(channel);
+%==========================================================================
+
+
+% %==============================PLOT RESULTS================================
+% figure
+% plot(parameter_range,success_measure(1,:),'g-*')
+% hold on
+% plot(parameter_range,success_measure(2,:),'k-o')
+% plot(parameter_range,acc_theory,'r')
+% plot(parameter_range,err_theory,'k')
+% title(['q=',num2str(q),' p=',num2str(p),' theta=',num2str(theta),' n=',num2str(n)])
+% legend('No. correct edges','No. false edges')
+% xlabel('T')
+% %==========================================================================
--- Neural_Network_Tomography/simple_Hebb_rule_leaky.m
+++ Neural_Network_Tomography/simple_Hebb_rule_leaky.m
@@ -1,4 +1,4 @@
-function simple_Hebb_rule_leaky(n,T,tau,theta,p,q,no_averaging_itrs
+function simple_Hebb_rule_leaky(n,T,tau,theta,p,q,no_averaging_itrs,B_LLR_thr,B_LLR_flag)
 
 %===========================INITIALIZATION=================================
 acc_ave = 0;                    % average number of correctly infered edges
@@ -10,6 +10,8 @@ no_edges_fire = ceil(theta*n);  % estimated number of edges required for a neuro
 success_measure = zeros(1,2);
 acc_theory = 0;
 err_theory = 0;
+fit_coeff_0 = [-0.0000    0.0002   -0.0041    0.0362   -0.1104];
+fit_coeff_1 = [0.0001   -0.0073    0.2498   -3.8715   24.0011];
 
 a=clock;                                % Initialize the seed for random number generation with the clock value.
 RandStream.setDefaultStream(RandStream('mt19937ar','seed',sum(100*a))); 
@@ -26,11 +28,8 @@ for av_itr = 1:no_averaging_itrs
     %----------------------------------------------------------------------
                 
     %------------------------Record Sample States--------------------------
-    S = zeros(T,n);                 % recorded stimulus states     
-    S2 = zeros(T,n);                 % recorded stimulus states     
-    R = zeros(T,1);                 % recorder output state    
-    R2 = zeros(T,1);                 % recorder output state    
-    t_fire = zeros(T,1);            % recorder fie time for the output neuron
+    S = zeros(T,n);                 % recorded stimulus states         
+    R = zeros(T,1);                 % recorder output state            
                 
     for i = 1:T    
         s = max(rand(1,n) < q,0);   % stimulus applied to n neurons            
@@ -38,103 +37,92 @@ for av_itr = 1:no_averaging_itrs
         denom = tau.^(0:1:i-t_last-1);                        
         h = sum(G*(S(i:-1:t_last+1,:)')./denom);
             
-        R(i) = 1*(h/n > theta);
-        R2(i) = R(i);                                                                                
+        R(i) = 1*(h/n > theta);        
     end    
     %----------------------------------------------------------------------
-        
-        %------------------Infer the Connectivity Matrix-------------------
-        W = zeros(1,n);
-        R2 = R;
-        ss = sum(S');
-        ss = ss-no_edges_fire;
-        ss = ss.*(ss>0);
-        ss = ss + .2;
-        
-        R = R - (R==0);
-%         R2 = R./ss';
                 
-%         S = S - (S==0)./(sum(S')'*ones(1,n))/p;        
-%         for i=1:n            
-%             W(i) = sum(S(:,i)'*R)/T;
-% %             temp = sum(S(:,i)'*t_fire)/T;
-%         end
-    for i = 1:n
+    %--------------------Infer the Connectivity Matrix---------------------        
+    W = zeros(1,n);
+    
+    for i = 1:n       
         t_last = 0;
         for jj = 1:T
             if (S(jj,i))
                 temp = 0;
                 for j = jj:-1:t_last+1
-%                     temp = temp+ ((-1)^(R2(jj)+S(j,i)))/tau2^(1*(jj-j));
                     temp = temp + S(j,i)/tau^(1*(jj-j));
                 end
                 L = sum(S(jj,:));
                 if (L > no_edges_fire)                    
-%                     B_LLR = calculate_belief_LLR(no_dges_fire,L,p);
-                    
-                    
-                    if (R2(jj))
+                    % B_LLR = calculate_belief_LLR(no_dges_fire,L,p);
+                                       
+                    if (R(jj))
                         B_LLR = polyval(fit_coeff_1,L);
-%                         if (B_LLR>1.5)
-                            W(i) = W(i) + temp;
-%                         end
+                        if (B_LLR>B_LLR_thr)
+                            if (B_LLR_flag)
+                                W(i) = W(i) + B_LLR * temp;
+                            else
+                                W(i) = W(i) + 1 * temp;
+                            end
+                        end
                     else
                         B_LLR = polyval(fit_coeff_0,L);
-%                         if (B_LLR>.15)
-                            W(i) = W(i) - temp;
-%                         end
+                        if (B_LLR>B_LLR_thr)
+                            if (B_LLR_flag)
+                                W(i) = W(i) - B_LLR * temp;
+                            else
+                                W(i) = W(i) - 1 * temp;
+                            end
+                        end
                     end
                     
-                end
-                if (R2(jj))
-                    t_last = jj;            
-                end
+                end                
             end
-        end
-        111;
-    end
-        W2 = W;
-        %------------------------------------------------------------------
+            if (R(jj))                    
+                t_last = jj;                            
+            end
+        end        
+    end    
+    W2 = W;        
+    %----------------------------------------------------------------------
 
-
-        %------------------Determine the Weight Threshold------------------
-        [W_sorted,ind] = sort(W);
-%         w_thr = determine_weight_thr_leaky(n,p,q,theta);
-%         w_thr = q*(1/avg_T_fire+1/avg_T_fire2)/2;
-%         W = (W>w_thr);
-        W = zeros(1,n);
-        W(ind(end-sum(G):end)) = abs(sign(W_sorted(end-sum(G):end)));
-        %------------------------------------------------------------------
-
-        
-        %--------------------Calculate the Accuracy-------------------------
-        acc = sum(W.*G);
-        err = abs(sum(sign(W+G)-W.*G));
-        acc_ave = acc_ave + acc/sum(G);
-        err_ave = err_ave + err/(n-sum(G));
-        %------------------------------------------------------------------
-
-    end
+    %--------------------Determine the Weight Threshold--------------------        
+    [W_sorted,ind] = sort(W);
+     % w_thr = determine_weight_thr_leaky(n,p,q,theta);
+     % w_thr = q*(1/avg_T_fire+1/avg_T_fire2)/2;
+     % W = (W>w_thr);        
+     
+     W = zeros(1,n);     
+     W(ind(end-sum(G):end)) = abs(sign(W_sorted(end-sum(G):end)));        
+     %---------------------------------------------------------------------
+                
+     %----------------------Calculate the Accuracy-------------------------
+     acc = sum(W.*G);        
+     err = abs(sum(sign(W+G)-W.*G));        
+     acc_ave = acc_ave + acc/sum(G);        
+     err_ave = err_ave + err/(n-sum(G));        
+     %---------------------------------------------------------------------
     
-    %-----------Calculate Theoretical Bounds and Store Results-------------
-    success_measure(1,itr) = acc_ave/av_itr;
-    success_measure(2,itr) = err_ave/av_itr;    
-%     [p1_T,p2_T] = simple_hebb_rule_theory_leaky(n,p,q,T,theta,tau,avg_T_fire);
-    simple_hebb_rule_theory(n,p,q,T,theta,0);
-    acc_theory(itr) =p1_T;
-    err_theory(itr) = p2_T;
-    %----------------------------------------------------------------------    
 end
 %==========================================================================
 
-%==============================PLOT RESULTS================================
-figure
-plot(parameter_range,success_measure(1,:),'g-*')
-hold on
-plot(parameter_range,success_measure(2,:),'k-o')
-plot(parameter_range,acc_theory,'r')
-plot(parameter_range,err_theory,'k')
-title(['q=',num2str(q),' p=',num2str(p),' theta=',num2str(theta),' n=',num2str(n)])
-legend('No. correct edges','No. false edges')
-xlabel('T')
+
+%===========================SAVE THE RESULTS===============================
+success_measure(1) = acc_ave/av_itr;
+success_measure(2) = err_ave/av_itr;    
+% [p1_T,p2_T] = simple_hebb_rule_theory_leaky(n,p,q,T,theta,tau,avg_T_fire);
+% simple_hebb_rule_theory(n,p,q,T,theta,0);    
+% acc_theory(itr) =p1_T;    
+% err_theory(itr) = p2_T;
+
+if (B_LLR_flag)
+    fid = fopen(['Belief_LLR_n_',num2str(n),'_no_averaging_itrs_',num2str(no_averaging_itrs),'.txt'], 'a+');        
+else
+    fid = fopen(['Belief_LLR_Delta_1_n_',num2str(n),'_no_averaging_itrs_',num2str(no_averaging_itrs),'.txt'], 'a+');        
+end
+fprintf(fid, 'T \t %d \t theta \t %f \t p \t %f \t q \t %f \t tau \t %f \t BBLR_thr \t %f \t acc \t %f \t err \t %f \t',T,theta,p,q,tau,B_LLR_thr,success_measure(1),success_measure(2));
+fprintf(fid,'\n');
+fclose(fid);
+    
 %==========================================================================
+
